46d605c174ba316fc7a4ad39e09847bc4cfdf3e1
VictoryIntel <63855072+VictoryIntel@users.noreply.github.com>
2020-10-14 12:56:27 +0300

    Added a Navigation bar

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+
+// Direct express to the public folder
+app.use(express.static("public"));
+// set the view engine to ejs
+app.set("view engine", "ejs");
+// list all the posts on pets
+app.get("/", (req, res)=>{
+    res.render("pages/posts")
+});
+
 app.listen(3000, ()=>{
     console.log("Server running")
-})
+});
