db6fa4bbecfe7264221d6943481a1d5103ec2e88
VictoryIntel <63855072+VictoryIntel@users.noreply.github.com>
2020-10-15 11:53:18 +0300

    Started again

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const app = express();
 
 // Direct express to the public folder
 app.use(express.static("public"));
+
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
+var posts = [
+    {title: "Widgets and You", author: "Kennedy Omondi", image: "https://images.unsplash.com/photo-1548767797-d8c844163c4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
+    {title: "How to Train Your Cat", author: "Salome Mberia", image: "https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
+    {title: "Don't Put That There", author: "Aisha Njenga", image: "https://images.unsplash.com/photo-1548767797-d8c844163c4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
+    {title: "Amateur Chainsaw Juggling", author: "Wycliff Mutobo", image: "https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
+    {title: "The Legend of the BeeGees", author: "Eusebius Likoko", image: "https://images.unsplash.com/photo-1548767797-d8c844163c4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"}
+]
+
+
 // list all the posts on pets
 app.get("/", (req, res)=>{
-    res.render("pages/posts")
+    res.render("pages/posts", {posts:posts, style:"app.css"})
 });
 
 app.listen(3000, ()=>{
