0320511497c4242a8e5e8878515e55a272fede4c
VictoryIntel <63855072+VictoryIntel@users.noreply.github.com>
2020-10-16 17:20:19 +0300

    Added date and subject to the postSchema

--- models/post.js
+++ models/post.js
@@ -7,7 +7,9 @@ let Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title:String,
     author: String,
-    image:String
+    image:String,
+    date:{type:Date, default:Date.now},
+    subject:{type:String, default:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque, debitis sit quae odio sed aliquid sapiente exercitationem non iste sunt repellendus amet eius alias laudantium atque similique accusantium vel. Totam ea quam vel ad eos expedita, explicabo minus esse sunt?"}
     
 });
 
